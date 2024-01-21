875af24951a5bad96d8cb9ff19eaa58434be5b61
KanatovAlikhan <cr67232@gmail.com>
2024-01-21 16:40:38 +0600

    Lab1

---
+++ lab1/DataTypes.py
@@ -0,0 +1,31 @@
+#Ex1
+x = 5
+print(type(x))
+#answer is int
+#Ex2
+x = "Hello World"
+print(type(x))
+#answer is str
+#Ex3
+x = 20.5
+print(type(x))
+#answer is float
+#Ex4 
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#answer is list
+#Ex5 
+x = ("apple", "banana", "cherry")
+print(type(x))
+#answer is tuple
+#Ex6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#answer is dict
+#Ex7 
+x = True
+print(type(x))
+#answer is bool
+
+
+
---
+++ lab1/Syntax.py
@@ -0,0 +1,5 @@
+#EX1
+print("Hello World")
+#EX2
+if 5 > 2:
+    print("YES")
