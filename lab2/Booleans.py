05450e12a64f9fecdd4c8bbb20e4f1dc21a7f195
KanatovAlikhan <cr67232@gmail.com>
2024-01-23 21:31:45 +0600

    Lab2

---
+++ lab2/Booleans.py
@@ -0,0 +1,17 @@
+#Ex1
+print(10 > 9)
+#answer is True
+#Ex2
+print(10 == 9)
+#answer is False
+#Ex3
+print(10 < 9)
+#answer is False
+#Ex4
+print(bool("abc"))
+#answer is True
+#Ex5
+print(bool(0))
+#answer is False
+
+
