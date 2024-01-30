60c6a9e39ad0b59e27616654b56ab111dd8c1662
KanatovAlikhan <cr67232@gmail.com>
2024-01-30 20:30:42 +0600

    Functions 2 done but 1 not (((

--- Lab3/Functions1.py
+++ Lab3/Functions1.py
@@ -30,12 +30,41 @@ for i in mylist:
     if c==1:
         myprimes.append(i)
 filter_prime(myprimes)
+#Ex 5 
+import itertools
+def perm():
+    for x in permutations:
+        print(x)
+s=str(input())
+permutations = list(itertools.permutations(s))
+perm()
+'''
+#Ex6////////////
+''' 
+s=str(input())
+#Ex7
+def has_33(nums):
+    ans=False
+    size=len(nums)
+    for i in range(size):
+        if nums[i]==3 and nums[i+1]==3:
+            ans=True
+            break
+    print(ans)
+has_33([1, 3, 3])
+'''
+#Ex8////////////////// 
+
+'''
 #Ex9
 import math
 def volume(r):
     ans=4/3 * 3.14 * math.pow(r,3)
     print(ans)
 volume(int(input("Write radius ")))
+'''
+#Ex10///////////
+'''
 #Ex11
 def palindrome(word):
     if(word == t):
@@ -51,4 +80,30 @@ mylist.reverse()
 for x in mylist:
     t+=x
 palindrome(s)
+#Ex12
+def histogram(nums):
+    for i in nums:
+        print("*"*i)
+histogram([4, 9, 7])
+#Ex13
+import random
+def guess():
+    print("Hello! What is your name?")
+    name=str(input())
+    cnt=0
+    print("Well,",name,", I am thinking of a number between 1 and 20.")
+    print("Take a guess.")
+    number=random.randint(1, 20)
+    a=int(input())
+    while number!=a:
+        if a>number:
+            print("Your guess is too high.")
+            print("Take a guess.")
+        else:
+            print("Your guess is too low.")
+            print("Take a guess.")
+        a=int(input())
+        cnt+=1
+    print("Good job,",name,"! You guessed my number in",cnt+1,"guesses!")
+guess()
 '''
