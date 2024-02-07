bf9346ffc15f10ebdd401729f6148c42807b5081
KanatovAlikhan <cr67232@gmail.com>
2024-02-07 10:44:45 +0600

    Lab4

---
+++ Lab4/generators.py
@@ -0,0 +1,38 @@
+#Ex1
+n=int(input())
+x=map(lambda x:x**2,range(1,n+1))
+for i in x:
+    print(i)
+#Ex2
+def even(n):
+    for i in range(n+1):
+        if i%2==0:
+            yield i
+n=int(input())
+ans=even(n)
+for i in ans:
+    print(i,end=" ")
+#Ex3
+def divisible(n):
+    for i in range(0,n+1):
+        if i%3==0 or i%4==0:
+            yield i
+n=int(input())
+ans=divisible(n)
+for i in ans:
+    print(i,end=" ")
+#Ex4
+def squares(a,b):
+    for i in range(a,b+1):
+        yield i**2
+ans=squares(int(input()),int(input()))
+for i in ans:
+    print(i,end=" ")
+#Ex5
+def fn(n):
+    while n>=0:
+        yield n
+        n-=1
+ans=fn(int(input()))
+for i in ans:
+    print(i,end=" ")
