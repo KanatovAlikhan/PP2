2bc7efe228b7980a901d609945af6cf100faf958
KanatovAlikhan <cr67232@gmail.com>
2024-03-11 14:10:20 +0600

    Lab7

--- Lab7/pyGame.py
+++ Lab7/pyGame.py
@@ -1,12 +1,17 @@
-import pygame
+import pygame 
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 500))
-done = False
-pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
-while not done:
+screen = pygame.display.set_mode((600,400))
+pygame.display.set_caption("Clock")
+screen.fill((17, 54, 27))
+done = True
+while done:
+        pygame.display.update()
+        pygame.draw.circle(screen,(112, 2, 2),(100,100),70)
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                        done = True
-        
-        pygame.display.flip()
+                        done = False
+                        pygame.quit()
+                elif event.type==pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE:
+                                screen.fill((14, 32, 48))
